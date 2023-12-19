71901a70acb0e0d38750d6602655fa9f51a8938e
Mohamed-Hammada <91836396+Mohamed-Hammada@users.noreply.github.com>
2023-12-19 17:32:48 +0200

    store theme in session storage

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
@@ -9,7 +9,7 @@ import { AsyncPipe, NgIf } from '@angular/common';
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css']
 })
-export class NavigationComponent {
+export class NavigationComponent implements OnInit {
   private breakpointObserver = inject(BreakpointObserver);
   isDarkMode: boolean = false;
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
@@ -18,14 +18,25 @@ export class NavigationComponent {
       shareReplay()
     );
 
+    ngOnInit() {
+      const storedTheme = sessionStorage.getItem('theme');
+      if (storedTheme) {
+        this.isDarkMode = storedTheme === 'dark';
+      }
+      this.toggleTheme();
+    }
+
+    
     toggleTheme() {
       this.isDarkMode = !this.isDarkMode;
       if (this.isDarkMode) {
         document.body.classList.add('theme-dark');
         document.body.classList.remove('theme-light'); // Fix the typo here
+        sessionStorage.setItem('theme', 'dark');
       } else {
         document.body.classList.add('theme-light');
         document.body.classList.remove('theme-dark');
+        sessionStorage.setItem('theme', 'light');
       }
     }
 }
