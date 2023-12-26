3f75a1d16f2212bb17737307b0865d55cf450d76
Mohamed-Hammada <91836396+Mohamed-Hammada@users.noreply.github.com>
2023-12-26 06:00:41 +0200

    fix use env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
-    production: true,
-    apiUrl: 'https://jsonplaceholder.typicode.com',
-  };
-  
+  production: true,
+  apiUrl: 'http://localhost:8080/clinical-childerns',
+};
